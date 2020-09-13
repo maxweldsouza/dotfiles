a2f43a8b54ed036fd026485ba663ecc1ed40c4c2
Maxwel Dsouza <maxweldsouza.in@gmail.com>
2020-09-13 23:40:32 +0530

    Update styling

--- Projects/codegen/components/Page.jsx
+++ Projects/codegen/components/Page.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
+    font-family: 'Source Sans Pro';
     max-width: 800px;
-    margin: 0 auto;
+    margin: 100px auto;
 `;
 
 function Page(props) {
--- Projects/codegen/pages/button.js
+++ Projects/codegen/pages/button.js
@@ -1,17 +1,23 @@
-import React,{useState} from 'react';
-import styled from 'styled-components';
-import {LiveEditor, LiveError, LivePreview, LiveProvider} from 'react-live'
+import React, { useState } from "react";
+import styled from "styled-components";
+import { LiveEditor, LiveError, LivePreview, LiveProvider } from "react-live";
 import Page from "../components/Page";
-import { ChromePicker } from 'react-color';
-
+import { ChromePicker } from "react-color";
+import Head from 'next/head';
 
 const editorStyle = {
-    'background': '#1e1e1e',
-    borderRadius: '3px'
+    background: "#1e1e1e",
+    borderRadius: "3px",
 };
 
 const MarginBottom = styled.div`
-    margin-bottom: 20px;
+    margin-bottom: ${({margin}) => margin || 15}px;
+`;
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    gap: 15px;
 `;
 
 function Button() {
@@ -43,21 +49,63 @@ render(<Button>Test</Button>)
 `;
     return (
         <Page>
+            <Head>
+                <title>
+                    Button
+                </title>
+            </Head>
             <h1>Button</h1>
-            Padding
-            <input type="range" min="1" max="200" value={paddingX} className="slider" id="myRange" onChange={e => setPaddingX(e.target.value)}/>
-            <input type="range" min="1" max="100" value={paddingY} className="slider" id="myRange" onChange={e => setPaddingY(e.target.value)}/>
-            Border Radius
-            <input type="range" min="0" max="20" value={borderRadius} className="slider" id="myRange" onChange={e => setBorderRadius(e.target.value)}/>
-            Font Size
-            <input type="range" min="0" max="50" value={fontSize} className="slider" id="myRange" onChange={e => setfontSize(e.target.value)}/>
-            <ChromePicker color={color} onChange={c => setcolor(c.hex)}/>
-            <LiveProvider code={code} scope={{styled}} noInline={true}>
+            <MarginBottom margin={50}>
+                <Grid>
+                    Padding X
+                    <input
+                        type="range"
+                        min="1"
+                        max="200"
+                        value={paddingX}
+                        className="slider"
+                        onChange={(e) => setPaddingX(e.target.value)}
+                    />
+                    Padding Y
+                    <input
+                        type="range"
+                        min="1"
+                        max="100"
+                        value={paddingY}
+                        className="slider"
+                        onChange={(e) => setPaddingY(e.target.value)}
+                    />
+                    Border Radius
+                    <input
+                        type="range"
+                        min="0"
+                        max="20"
+                        value={borderRadius}
+                        className="slider"
+                        onChange={(e) => setBorderRadius(e.target.value)}
+                    />
+                    Font Size
+                    <input
+                        type="range"
+                        min="0"
+                        max="50"
+                        value={fontSize}
+                        className="slider"
+                        onChange={(e) => setfontSize(e.target.value)}
+                    />
+                    Background
+                    <ChromePicker
+                        color={color}
+                        onChange={(c) => setcolor(c.hex)}
+                    />
+                </Grid>
+            </MarginBottom>
+            <LiveProvider code={code} scope={{ styled }} noInline={true}>
                 <MarginBottom>
-                <LiveEditor style={editorStyle}/>
+                    <LiveEditor style={editorStyle} />
                 </MarginBottom>
-                <LiveError/>
-                <LivePreview/>
+                <LiveError />
+                <LivePreview />
             </LiveProvider>
         </Page>
     );
