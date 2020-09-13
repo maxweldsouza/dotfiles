d370e557979a6fca8385c6d32e654f83f077384a
Maxwel Dsouza <maxweldsouza.in@gmail.com>
2020-09-13 21:11:41 +0530

    Button generator

--- Projects/codegen/pages/button.js
+++ Projects/codegen/pages/button.js
@@ -1,12 +1,6 @@
-import React, {Component, useEffect, useState} from 'react';
-import * as Babel from '@babel/standalone'
+import React,{useState} from 'react';
 import styled from 'styled-components';
-import {
-    LiveProvider,
-    LiveEditor,
-    LiveError,
-    LivePreview
-} from 'react-live'
+import {LiveEditor, LiveError, LivePreview, LiveProvider} from 'react-live'
 import Page from "../components/Page";
 
 
@@ -15,19 +9,54 @@ const editorStyle = {
     borderRadius: '3px'
 };
 
-class Button extends Component {
-    render() {
-        return (
-            <Page>
-                <h1>Button</h1>
-                <LiveProvider code="<strong>Hello World!</strong>">
-                    <LiveEditor style={editorStyle}/>
-                    <LiveError />
-                    <LivePreview />
-                </LiveProvider>
-            </Page>
-        );
+const MarginBottom = styled.div`
+    margin-bottom: 20px;
+`;
+
+function Button() {
+    const [paddingX, setPaddingX] = useState(10);
+    const [paddingY, setPaddingY] = useState(10);
+    const [borderRadius, setBorderRadius] = useState(0);
+    const [fontSize, setfontSize] = useState(12);
+    const code = `
+const Button = styled.button\`
+    border-radius: ${borderRadius}px;
+    border: none;
+    box-shadow: none;
+    color: red;
+    cursor: pointer;
+    padding: ${paddingY}px ${paddingX}px;
+    font-size: ${fontSize}px;
+    &:disabled {
+        cursor: default;
+    }    
+    &:focus {
+        outline: dashed 2px red;
     }
+    
+\`
+
+render(<Button>Test</Button>)
+`;
+    return (
+        <Page>
+            <h1>Button</h1>
+            Padding
+            <input type="range" min="1" max="200" value={paddingX} className="slider" id="myRange" onChange={e => setPaddingX(e.target.value)}/>
+            <input type="range" min="1" max="100" value={paddingY} className="slider" id="myRange" onChange={e => setPaddingY(e.target.value)}/>
+            Border Radius
+            <input type="range" min="0" max="20" value={borderRadius} className="slider" id="myRange" onChange={e => setBorderRadius(e.target.value)}/>
+            Font Size
+            <input type="range" min="0" max="50" value={fontSize} className="slider" id="myRange" onChange={e => setfontSize(e.target.value)}/>
+            <LiveProvider code={code} scope={{styled}} noInline={true}>
+                <MarginBottom>
+                <LiveEditor style={editorStyle}/>
+                </MarginBottom>
+                <LiveError/>
+                <LivePreview/>
+            </LiveProvider>
+        </Page>
+    );
 }
 
 export default Button;
